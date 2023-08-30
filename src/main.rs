89860f3f8fd2dc8905de5e94ca1d01c0d0c20197
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2023-08-29 19:32:59 -0500

    Switch to async, add working Sleeper client

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod sleeper;
 
+#[tokio::main]
+async fn main() -> Result<(), sleeper::Error> {
+    let league_id = match std::env::args().nth(1) {
+        Some(l) => l,
+        None    => panic!("D'oh! You forgot to pass your league ID to the command line")
+    };
 
-fn main() {}
+    let client = sleeper::Client::new();
+    let resp_body = client.get_league(&league_id).await;
+
+    println!("{:?}", resp_body);
+
+    Ok(())
+}
 
--- src/sleeper.rs
+++ src/sleeper.rs
@@ -1,11 +1,8 @@
-use std::collections::HashMap;
-use std::fmt;
-use http::Version;
 use serde::Deserialize;
 
 pub type LeagueId = String;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, serde::Serialize)]
 pub struct League {
     pub total_rosters: u8,
     pub status: String,
@@ -20,11 +17,11 @@ pub struct League {
     pub name: String,
     pub metadata: LeagueMetadata,
     pub loser_bracket_id: Option<String>,
-    pub league_id: String,
+    pub league_id: LeagueId,
     pub last_transation_id: Option<String>,
     pub last_read_id: Option<String>,
     pub last_pinned_message_id: Option<String>,
-    pub las_message_time: u64,
+    pub last_message_time: u64,
     pub last_message_text_mape: Option<String>,
     pub last_message_id: Option<String>,
     pub last_message_attachment: Option<String>,
@@ -34,22 +31,22 @@ pub struct League {
     pub last_author_avatar: Option<String>,
     pub group_id: Option<String>,
     pub draft_id: Option<String>,
-    pub display_order: u8,
+    // pub display_order: u8,
     pub company_id: Option<String>,
     pub bracket_id: Option<String>,
     pub avatar: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, serde::Serialize)]
 pub struct LeagueMetadata {
     pub latest_league_winner_roster_id: Option<String>,
     pub keeper_deadline: String,
-    pub auto_contineu: String,
+    pub auto_continue: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, serde::Serialize)]
 pub struct LeagueSettings {
-    pub daily_waivers_last_run: u16,
+    pub daily_waivers_last_ran: u16,
     pub reserve_allow_cov: u8,
     pub reserve_slots: u8,
     pub leg: u8,
@@ -59,7 +56,7 @@ pub struct LeagueSettings {
     pub league_average_match: u8,
     pub waiver_type: u8,
     pub max_keepers: u8,
-    #[serde(rename(serialize = "type", deserialize = "_type"))]
+    #[serde(rename(serialize = "type", deserialize = "type"))]
     pub _type: u8,
     pub pick_trading: u8,
     pub disable_trades: u8,
@@ -97,55 +94,55 @@ pub struct LeagueSettings {
     pub best_ball: u8
 }
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct ScoringSettings {
-    pub st_ff: i8,
-    pub pts_allow_7_13: i8,
-    pub def_st_ff: i8,
+    pub st_ff: f64,
+    pub pts_allow_7_13: f64,
+    pub def_st_ff: f64,
     pub rec_yd: f64,
-    pub fum_rec_td: i8,
-    pub pts_allow_35p: i8,
-    pub pts_allow_28_34: i8,
-    pub fum: i8,
+    pub fum_rec_td: f64,
+    pub pts_allow_35p: f64,
+    pub pts_allow_28_34: f64,
+    pub fum: f64,
     pub rush_yd: f64,
-    pub pass_td: i8,
-    pub blk_kick: i8,
+    pub pass_td: f64,
+    pub blk_kick: f64,
     pub pass_yd: f64,
-    pub safe: i8,
-    pub def_td: i8,
-    pub fgm_50p: i8,
-    pub def_st_td: i8,
-    pub fum_rec: i8,
-    pub rush_2pt: i8,
-    pub xpm: i8,
-    pub pts_allow_21_27: i8,
-    pub fgm_20_29: i8,
-    pub pts_allow_1_6: i8,
-    pub fum_lost: i8,
-    pub def_st_fum_rec: i8,
-    pub int: i8,
-    pub def_kr_td: i8,
-    pub fgm_0_19: i8,
-    pub pts_allow_14_20: i8,
-    pub rec: i8,
-    pub ff: i8,
-    pub fgmiss: i8,
-    pub st_fum_rec: i8,
-    pub rec_2pt: i8,
-    pub def_pr_td: i8,
-    pub rush_td: i8,
-    pub xpmiss: i8,
-    pub fgm_30_39: i8,
-    pub rec_td: i8,
-    pub st_td: i8,
-    pub pass_2pt: i8,
-    pub pts_allow_0: i8,
-    pub pass_int: i8,
-    pub fgm_40_49: i8,
-    pub sack: i8,
+    pub safe: f64,
+    pub def_td: f64,
+    pub fgm_50p: f64,
+    pub def_st_td: f64,
+    pub fum_rec: f64,
+    pub rush_2pt: f64,
+    pub xpm: f64,
+    pub pts_allow_21_27: f64,
+    pub fgm_20_29: f64,
+    pub pts_allow_1_6: f64,
+    pub fum_lost: f64,
+    pub def_st_fum_rec: f64,
+    pub int: f64,
+    pub def_kr_td: f64,
+    pub fgm_0_19: f64,
+    pub pts_allow_14_20: f64,
+    pub rec: f64,
+    pub ff: f64,
+    pub fgmiss: f64,
+    pub st_fum_rec: f64,
+    pub rec_2pt: f64,
+    pub def_pr_td: f64,
+    pub rush_td: f64,
+    pub xpmiss: f64,
+    pub fgm_30_39: f64,
+    pub rec_td: f64,
+    pub st_td: f64,
+    pub pass_2pt: f64,
+    pub pts_allow_0: f64,
+    pub pass_int: f64,
+    pub fgm_40_49: f64,
+    pub sack: f64,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub enum RosterPosition {
     QB,
     RB,
@@ -164,24 +161,24 @@ pub struct Error {}
 
 pub struct Client {
     base_url: String,
-    client: reqwest::blocking::Client
+    client: reqwest::Client
 }
 
 impl Client {
     pub fn new() -> Self {
         Client {
             base_url: String::from("https://api.sleeper.app/v1"),
-            client: reqwest::blocking::Client::builder()
+            client: reqwest::Client::builder()
                 .https_only(true)
                 .build()
                 .unwrap()
         }
     }
 
-    pub fn get_league(&self, id: &str) -> Result<League, Error> {
+    pub async fn get_league(&self, id: &str) -> Result<League, Error> {
         let url = format!("{}/league/{}", &self.base_url, &id);
 
-        let res = match self.client.get(&url).send() {
+        let res = match self.client.get(&url).send().await {
             Ok(res) => res,
             Err(_) => {
                 eprintln!("Error while sending GET request to '{}'", &url);
@@ -189,7 +186,7 @@ impl Client {
             }
         };
 
-        let league: League = match res.json() {
+        let league: League = match res.json::<League>().await {
             Ok(league) => league,
             Err(_) => {
                 eprintln!("Error while deserializing League from response body");
@@ -198,13 +195,6 @@ impl Client {
         };
 
         Ok(league)
-    }
-}
-
-mod tests {
-    #[test]
-    fn test_sleeper_client_works() -> () {
-        let c = super::Client::new();
     }
 }
 
