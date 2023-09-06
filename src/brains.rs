2696b54aa75f6a3ed3fa0ff7b7b851c0c06520b8
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2023-09-05 22:24:30 -0500

    Add method to check rosters for inactive players

--- src/brains.rs
+++ src/brains.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use sleeper::data::{ InjuryStatus, AllPlayers, PlayerId, NflPlayer };
+use sleeper::data::{ InjuryStatus, AllPlayers, PlayerId, NflPlayer, Roster, OwnerId };
 use std::{collections::HashMap, slice::Iter};
 
 pub enum BotError {
@@ -24,10 +24,21 @@ pub struct InjuryWarningData {
     owner: OwnerInfo
 }
 
+/// Predicate used to evaluate what will be considered injured
+fn is_injured(st: InjuryStatus) -> Option<InjuryStatus> {
+    match st {
+        // if you're healthy you're not injured
+        InjuryStatus::Healthy | InjuryStatus::Questionable => None,
+        // Otherwise, you're injured (or suspended, or have COVID, or whatever else)
+        otherwise => Some(otherwise)
+    }
+}
+
+
 // / Given a roster, get a Vector of their injured players (of size 0 if they have none).
 // / Returns errors for various invalid states
 pub fn injured_from_starters(starters: Vec<PlayerId>, 
-                             players: &AllPlayers
+                             players: &AllPlayers,
 ) -> Result<Vec<(&NflPlayer, InjuryStatus)>, BotError> {
 
     let result: Vec<(&NflPlayer, InjuryStatus)> = starters.into_iter()
@@ -39,11 +50,26 @@ pub fn injured_from_starters(starters: Vec<PlayerId>,
             }
         }).filter_map(|ply: &NflPlayer| {
             match InjuryStatus::from_opt_string(ply.injury_status.clone()) {
-                Ok(st) => Some((ply, st)),
+                Ok(st) => match is_injured(st) {
+                    Some(injury) => Some((ply, injury)),
+                    None => None
+                },
                 Err(_) => None
             }
         }).collect::<Vec<(&NflPlayer, InjuryStatus)>>();
        
     Ok(result)
+}
+
+/// Checks the provided rosters for starters who are injured or unlikely to play, returning a list
+/// of tuples mapping an OwnerId (String) to their list of injured players. The list can be empty.
+pub fn check_rosters<'a>(rosters: &Vec<Roster>, all_players: &'a AllPlayers) -> Vec<(OwnerId, Vec<(&'a NflPlayer, InjuryStatus)>)> {
+    rosters.iter()
+        .filter_map(|rost| {
+            match injured_from_starters(rost.starters.to_owned(), all_players) {
+                Ok(injured_players) => Some((rost.owner_id.to_owned(), injured_players)),
+                Err(_) => None
+            }
+        }).collect()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use sleeper::client::Client;
 use sleeper::data::*;
-// use groupme::client::GroupmeClient;
 mod brains;
-
-type OwnerId = String;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -15,17 +12,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client  = Client::new();
 
-    // let owners = client.get_users_in_league(&league_id).await.expect("Could not get owners. Request was not completed");
     let rosters: Vec<Roster> = client.get_rosters(&league_id).await.expect("Could not get rosters. Request was not completed");
-    let players: AllPlayers = client.get_all_players(SleeperSport::NFL).await.expect("Could not get all NFL players. Request was not completed");
+    let all_players: AllPlayers = client.get_all_players(SleeperSport::NFL).await.expect("Could not get all NFL players. Request was not completed");
 
-    let result: Vec<(OwnerId, Vec<(&NflPlayer, InjuryStatus)>)> = rosters.iter()
-        .filter_map(|rost| {
-            match brains::injured_from_starters(rost.starters.to_owned(), &players) {
-                Ok(injured_players) => Some((rost.owner_id.to_owned(), injured_players)),
-                Err(_) => None
-            }
-        }).collect();
+    let result = brains::check_rosters(&rosters, &all_players);
 
     for value in &result {
         println!("Owner: {}", value.0);
