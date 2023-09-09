b499ce7078c8f137e5b9fe6ca5c5e7126c59a676
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2023-09-09 16:46:40 -0500

    Add roster starter checks for injuries, empties, and bye weeks

--- src/brains.rs
+++ src/brains.rs
@@ -1,11 +1,144 @@
+use std::collections::HashMap;
 use sleeper::data::{ InjuryStatus, AllPlayers, PlayerId, NflPlayer,
     Roster, OwnerId, RosterPosition, };
+
+/// Type representing a starter RosterPosition which was found to be empty
+pub type EmptySpot = RosterPosition;
+
+/// Data struct representing a player who is starting,
+/// despite their status indicating an injury that will
+/// prevent them from playing in the current week.
+pub struct InjuredStarter {
+    pub name: String,
+    pub status: InjuryStatus,
+    pub pos: RosterPosition
+}
+
+/// Data struct representing a player who is starting,
+/// despite being on bye for the current week.
+pub struct ByeStarter {
+    pub name: String,
+    pub pos: RosterPosition
+}
+
+/// Type to represent all of a specific fantasy team's invalid starters,
+/// including starters who are injured, on bye, or empty.
+pub type BadStarters = (Vec<InjuredStarter>, Vec<EmptySpot>, Vec<ByeStarter>);
 
 #[derive(Debug)]
 pub enum BotError {
     InjuryStatusNotFound,
     InvalidStarterIndex,
     Generic // remove
+}
+
+pub enum NflTeamAbbrv {
+    ARI, ATL, BAL, BUF,
+    CAR, CHI, CIN, CLE,
+    DAL, DEN, DET, GB,
+    HOU, IND, JAX, KC,
+    LAC, LAR, LV,  MIA,
+    MIN, NE,  NO,  NYG,
+    NYJ, PHI, PIT, SEA,
+    SF,  TB,  TEN, WAS,
+    FA // Not on a team
+}
+
+impl NflTeamAbbrv {
+    // TODO - Network call to make this current year-after-year w/o human intervention
+    /// Get the bye week for this team in the 2023 season. Week is literal value,
+    /// not zero-indexed (ie week 1 is literally the first week of the season)
+    pub fn get_bye_week(&self) -> u8 {
+        match self {
+            Self::CLE | Self::LAC | Self::SEA | Self::TB  => 5,
+            Self::GB  | Self::PIT => 6,
+            Self::CAR | Self::CIN | Self::DAL | Self::HOU | Self::NYJ | Self::TEN => 7,
+            Self::DEN | Self::DET | Self::JAX | Self::SF  =>  9,
+            Self::KC  | Self::LAR | Self::MIA | Self::PHI => 10,
+            Self::ATL | Self::IND | Self::NE  | Self::NO  => 11,
+            Self::BAL | Self::BUF | Self::CHI | Self::LV  | Self::MIN | Self::NYG => 13,
+            Self::ARI | Self::WAS => 14,
+            Self::FA  => 0 // Don't call this on a free agent
+        }
+    }
+
+    pub fn is_on_bye(&self, current_week: u8) -> bool {
+        match self {
+            Self::FA  => true, // All free agents are always 'on bye', since they aren't playing
+            unmatched => unmatched.get_bye_week() == current_week
+        }
+    }
+
+    /// Takes an optional team name, where 'None' means 'free agent'
+    pub fn new(team: Option<String>) -> Option<Self> {
+        match team {
+            None => Some(Self::FA),
+
+            Some(team_name) => match team_name.as_str() {
+                "ARI" => Some(NflTeamAbbrv::ARI),
+                "ATL" => Some(NflTeamAbbrv::ATL),
+                "BAL" => Some(NflTeamAbbrv::BAL),
+                "BUF" => Some(NflTeamAbbrv::BUF),
+                "CAR" => Some(NflTeamAbbrv::CAR),
+                "CHI" => Some(NflTeamAbbrv::CHI),
+                "CIN" => Some(NflTeamAbbrv::CIN),
+                "CLE" => Some(NflTeamAbbrv::CLE),
+                "DAL" => Some(NflTeamAbbrv::DAL),
+                "DEN" => Some(NflTeamAbbrv::DEN),
+                "DET" => Some(NflTeamAbbrv::DET),
+                "GB"  => Some(NflTeamAbbrv::GB),
+                "HOU" => Some(NflTeamAbbrv::HOU),
+                "IND" => Some(NflTeamAbbrv::IND),
+                "JAX" => Some(NflTeamAbbrv::JAX),
+                "KC"  => Some(NflTeamAbbrv::KC),
+                "LAC" => Some(NflTeamAbbrv::LAC),
+                "LAR" => Some(NflTeamAbbrv::LAR),
+                "LV"  => Some(NflTeamAbbrv::LV),
+                "MIA" => Some(NflTeamAbbrv::MIA),
+                "MIN" => Some(NflTeamAbbrv::MIN),
+                "NE"  => Some(NflTeamAbbrv::NE),
+                "NO"  => Some(NflTeamAbbrv::NO),
+                "NYG" => Some(NflTeamAbbrv::NYG),
+                "NYJ" => Some(NflTeamAbbrv::NYJ),
+                "PHI" => Some(NflTeamAbbrv::PHI),
+                "PIT" => Some(NflTeamAbbrv::PIT),
+                "SEA" => Some(NflTeamAbbrv::SEA),
+                "SF"  => Some(NflTeamAbbrv::SF),
+                "TB"  => Some(NflTeamAbbrv::TB),
+                "TEN" => Some(NflTeamAbbrv::TEN),
+                "WAS" => Some(NflTeamAbbrv::WAS),
+                "FA"  => Some(NflTeamAbbrv::FA),
+                _     => None,
+            }
+        }
+    }
+}
+
+/// Checks the provided rosters for starters who are injured or unlikely to play, returning a list
+/// of tuples mapping an OwnerId (String) to their list of bad starters. The list can be empty.
+pub fn check_rosters<'a>(rosters: &Vec<Roster>, 
+                         all_players: &'a HashMap<PlayerId, NflPlayer>, 
+                         current_week: u8
+) -> Vec<(OwnerId, BadStarters)> {
+
+    rosters.iter().map(|roster: &Roster|{
+        let starter_ids: Vec<PlayerId> = roster.starters.clone();
+
+        // Collect starters into vec of option, because if we filter out the players
+        // we can't get from the map, we are removing the ID "0" (empty players),
+        // and thus fucking up the order / quantity of starters. We need to preserve order
+        // because the order determines which position the starter is in at.
+        let starters: Vec<Option<&NflPlayer>> = starter_ids.iter()
+            .map(|id| all_players.get(id))
+            .collect();
+
+        let empty_starters:   Vec<EmptySpot>      = empties_from_starters(&roster.starters);
+        let injured_starters: Vec<InjuredStarter> = injured_from_starters(&starters);
+        let bye_starters:     Vec<ByeStarter>     = byes_from_starters(&starters, current_week);
+
+        (roster.owner_id.to_owned(), ( injured_starters, empty_starters, bye_starters ))
+
+    }).collect()
 }
 
 /// Predicate used to evaluate what will be considered injured
@@ -18,38 +151,26 @@ fn is_injured(st: InjuryStatus) -> Option<InjuryStatus> {
     }
 }
 
-// / Given a roster, get a Vector of their injured players (of size 0 if they have none).
-// / Returns errors for various invalid states
-pub fn injured_from_starters(starters: Vec<PlayerId>, 
-                             players: &AllPlayers) -> Result<Vec<(&NflPlayer, InjuryStatus)>, BotError> {
-    let result: Vec<(&NflPlayer, InjuryStatus)> = starters.into_iter()
-        .filter_map(|p: PlayerId| {
-            match players {
-                AllPlayers::NFL(all_nfl) => all_nfl.get(&p).map_or(None, |starter_info| Some(starter_info)),
-                AllPlayers::LCS(_) => todo!(),
-                AllPlayers::NBA(_) => todo!()
-            }
-        }).filter_map(|ply: &NflPlayer| {
-            match InjuryStatus::from_opt_string(ply.injury_status.clone()) {
-                Ok(st) => match is_injured(st) {
-                    Some(injury) => Some((ply, injury)),
-                    None => None
+/// Given a roster, get a Vector of their injured players (of size 0 if they have none).
+pub fn injured_from_starters(starters: &Vec<Option<&NflPlayer>>) -> Vec<InjuredStarter> {
+    starters.iter()
+        .enumerate()
+        .filter_map(|(index, opt_player)| {
+            match opt_player {
+                Some(player) => match InjuryStatus::from_opt_string(player.injury_status.clone()) {
+                    Ok(status) => match is_injured(status) {
+                        Some(injury) => {
+                            Some(InjuredStarter {
+                                name: player.full_name.as_ref().unwrap().to_string(),
+                                pos: position_from_index(index).unwrap(),
+                                status: injury
+                            })
+                        },
+                        None => None
+                    },
+                    Err(_) => None
                 },
-                Err(_) => None
-            }
-        }).collect::<Vec<(&NflPlayer, InjuryStatus)>>();
-       
-    Ok(result)
-}
-
-/// Checks the provided rosters for starters who are injured or unlikely to play, returning a list
-/// of tuples mapping an OwnerId (String) to their list of injured players. The list can be empty.
-pub fn check_rosters<'a>(rosters: &Vec<Roster>, all_players: &'a AllPlayers) -> Vec<(OwnerId, Vec<(&'a NflPlayer, InjuryStatus)>)> {
-    rosters.iter()
-        .filter_map(|rost| {
-            match injured_from_starters(rost.starters.to_owned(), all_players) {
-                Ok(injured_players) => Some((rost.owner_id.to_owned(), injured_players)),
-                Err(_) => None
+                None => None
             }
         }).collect()
 }
@@ -57,7 +178,7 @@ pub fn check_rosters<'a>(rosters: &Vec<Roster>, all_players: &'a AllPlayers) -> 
 /// Given a vector of PlayerId's representing the starters,
 /// produces a vector of the RosterPositions for which the PlayerId
 /// was "0" (signifying empty).
-pub fn empties_from_starters(starters: Vec<PlayerId>) -> Vec<RosterPosition> {
+pub fn empties_from_starters(starters: &Vec<PlayerId>) -> Vec<EmptySpot> {
     let mut result: Vec<RosterPosition> = vec![];
 
     for (index, value) in starters.iter().enumerate() {
@@ -68,6 +189,31 @@ pub fn empties_from_starters(starters: Vec<PlayerId>) -> Vec<RosterPosition> {
     }
 
     result
+}
+
+/// Produce a list of ByeStarters from one fantasy team's starter list.
+pub fn byes_from_starters(starters: &Vec<Option<&NflPlayer>>, current_week: u8) -> Vec<ByeStarter> {
+    starters.iter()
+        .enumerate()
+        .filter_map(|(index, opt_player)| {
+            match opt_player {
+                Some(player) => match NflTeamAbbrv::new(player.team.clone()) {
+                    Some(team) => match team.is_on_bye(current_week) {
+                        true => Some(ByeStarter {
+                            name: player.full_name.clone().unwrap().to_string(),
+                            pos:  position_from_index(index).unwrap()
+                        }),
+                        false => None
+                    },
+                    // No team, meaning the player is a free agent and therefore won't be playing
+                    None => Some(ByeStarter {
+                        name: player.full_name.clone().unwrap().to_string(),
+                        pos:  position_from_index(index).unwrap()
+                    })
+                },
+                None => None
+            }
+        }).collect()
 }
 
 // TODO - determine a way to make this generic, using the league_settings field of league
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,62 @@
+use std::collections::HashMap;
 use sleeper::client::Client;
 use sleeper::data::*;
 mod brains;
+use brains::*;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let league_id = std::env::args()
+                             .nth(1)
+                             .expect("No league ID was passed to the executable!");
 
-    let league_id = match std::env::args().nth(1) {
-        Some(id) => id,
-        None => panic!("No league ID was passed to the executable!")
-    };
+    let client = Client::new();
 
-    let client  = Client::new();
-
-    let rosters: Vec<Roster> = client.get_rosters(&league_id).await.expect("Could not get rosters. Request was not completed");
     let all_players: AllPlayers = client.get_all_players(SleeperSport::NFL).await.expect("Could not get all NFL players. Request was not completed");
-
-    let result = brains::check_rosters(&rosters, &all_players);
-
-    for value in &result {
-        println!("Owner: {}", value.0);
-        println!("Injured players: {}", value.1.len());
+    let all_nfl_players: HashMap<PlayerId, NflPlayer> = match all_players {
+        AllPlayers::NFL(players) => players,
+        _ => panic!("Wrong sport! This is a fantasy football bot :)")
     };
+
+    let rosters: Vec<Roster>    = client.get_rosters(&league_id).await.expect("Could not get rosters. Request was not completed");
+    let nfl_state: SportState   = client.get_sport_state(SleeperSport::NFL)
+                                        .await.expect("Could not get NFL sport state. Request was not completed");
+
+    let current_week            = nfl_state.week;
+
+    let result: Vec<(OwnerId, BadStarters)> = brains::check_rosters(&rosters, &all_nfl_players, current_week);
+
+    result.iter().for_each(|value| {
+        println!("OwnerID: {}", value.0);
+        if value.1.0.len() > 0 {
+            println!("->  Injured starters...");
+            for inj in &value.1.0 {
+                println!("    ->  {} - {} is {}", inj.pos, inj.name, inj.status);
+            }
+        } else {
+            println!("->  No injured starters!");
+        }
+
+
+        if value.1.1.len() > 0 {
+            println!("->  Empty starters...");
+            for empty in &value.1.1 {
+                println!("    ->  {} is empty", empty);
+            }
+        } else {
+            println!("->  No empty starters!");
+        }
+
+        if value.1.2.len() > 0 {
+            println!("->  Starters on bye...");
+            for bye in &value.1.2 {
+                println!("    ->  {} - {} is on bye", bye.pos, bye.name);
+            }
+        } else {
+            println!("->  No starters on bye!");
+        }
+    });
 
     Ok(())
 }
