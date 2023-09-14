7c168dfa3436cfac8a7cedf8f7e85371d02e2ba9
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2023-09-14 16:25:48 -0500

    WIP - Add asynchronous task schedule for roster checks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::str::FromStr;
+use std::sync::Arc;
 use chrono::Utc;
 use cron::Schedule;
+use groupme::client::GroupmeClient;
 use tokio::time::{self, Duration};
 use sleeper::client::Client as SleeperClient;
 use sleeper::data::*;
@@ -17,35 +19,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let league_id = std::env::args()
                              .nth(1)
                              .expect("No league ID was passed to the executable!");
+    let bot_id = std::env::args()
+                          .nth(2)
+                          .expect("No bot ID was passed to the executable!");
 
-    let sunday_before_noon        = Schedule::from_str("0 0 11 * * SUN").expect("Invalid cron string!");
-    let monday_thursday_primetime = Schedule::from_str("0 30 18 * * MON,THURS").expect("Invalid cron string!");
+    // let sunday_before_noon        = Schedule::from_str("0 0 11 * * SUN").expect("Invalid cron string!");
+    // let monday_thursday_primetime = Schedule::from_str("0 30 18 * * MON,THURS").expect("Invalid cron string!");
 
     let sleeper_client = SleeperClient::new();
 
-    loop {
-        let now = Utc::now();
-        println!("Now {}", now);
+    let result = check_rosters_and_message(league_id.as_ref(), &sleeper_client, bot_id.as_ref()).await;
 
-        let next_before_primetime = monday_thursday_primetime.after(&now)
-            .next()
-            .expect("Failed to find next run for Monday & Thursday primetime!");
+    Ok(())
 
-        let next_before_noon = sunday_before_noon.after(&now)
-            .next()
-            .expect("Failed to find next run for Sunday before noon!");
+    // loop {
+    //     let now = Utc::now();
+    //     println!("Now {}", now);
 
-        let until_next_sun_noon = next_before_noon.signed_duration_since(now);
-        let until_next_mon_thurs_prime = next_before_primetime.signed_duration_since(now);
-        let min_duration = until_next_sun_noon.min(until_next_mon_thurs_prime);
+    //     let next_before_primetime = monday_thursday_primetime.after(&now)
+    //         .next()
+    //         .expect("Failed to find next run for Monday & Thursday primetime!");
 
-        time::sleep(min_duration.to_std().unwrap()).await;
+    //     let next_before_noon = sunday_before_noon.after(&now)
+    //         .next()
+    //         .expect("Failed to find next run for Sunday before noon!");
 
-        check_rosters_and_message(league_id.as_ref(), &sleeper_client).await;
-    }
+    //     let until_next_sun_noon = next_before_noon.signed_duration_since(now);
+    //     let until_next_mon_thurs_prime = next_before_primetime.signed_duration_since(now);
+    //     let min_duration = until_next_sun_noon.min(until_next_mon_thurs_prime);
+
+    //     time::sleep(min_duration.to_std().unwrap()).await;
+
+    //     check_rosters_and_message(league_id.as_ref(), &sleeper_client, bot_id.as_ref()).await;
+    // }
 }
 
-async fn check_rosters_and_message(league_id: &str, sleeper_client: &SleeperClient) -> () {
+async fn check_rosters_and_message(league_id: &str, sleeper_client: &SleeperClient, bot_id: &str) -> () {
+    let msg_client = GroupmeClient::new(bot_id.to_string());
+
     let all_players: AllPlayers = sleeper_client.get_all_players(SleeperSport::NFL).await.expect("Could not get all NFL players. Request was not completed");
     let all_nfl_players: HashMap<PlayerId, NflPlayer> = match all_players {
         AllPlayers::NFL(players) => players,
@@ -53,44 +64,107 @@ async fn check_rosters_and_message(league_id: &str, sleeper_client: &SleeperClie
     };
 
     let rosters: Vec<Roster>  = sleeper_client.get_rosters(&league_id).await.expect("Could not get rosters. Request was not completed");
+    let owners: Vec<SleeperUser> = sleeper_client.get_users_in_league(&league_id).await.expect("Could not get owners in league. Request was not completed");
     let nfl_state: SportState = sleeper_client.get_sport_state(SleeperSport::NFL).await.expect("Could not get NFL sport state. Request was not completed");
 
     let current_week = nfl_state.week;
 
-    let result: Vec<(OwnerId, BadStarters)> = brains::check_rosters(&rosters, &all_nfl_players, current_week);
+    let result: Box<Vec<(SleeperUser, BadStarters)>> = Box::new(brains::check_rosters(&rosters, &all_nfl_players, current_week)
+        .into_iter()
+        .map(|value| {
+            let owner = owners.iter().find(|user| user.user_id == value.0).unwrap();
+            (owner.to_owned(), value.1)
+        }).collect());
 
-    print_invalid_starters(result);
-}
+    println!("Got result");
 
-fn print_invalid_starters(values: Vec<(OwnerId, BadStarters)>) -> () {
-    values.iter().for_each(|value| {
-        println!("OwnerID: {}", value.0);
-        if value.1.0.len() > 0 {
-            println!("->  Injured starters...");
-            for inj in &value.1.0 {
-                println!("    ->  {} - {} is {}", inj.pos, inj.name, inj.status);
+    let tasks = result.into_iter().map(|(user, bad_starters)| {
+        println!("Declaring a new task");
+        let bad_starters_box = Box::new(bad_starters);
+        // let metadata  = user.metadata.clone();
+        // let team_name = metadata.get("team_name").unwrap();
+
+        let username  = user.display_name.unwrap();
+        let msg_client_copy = msg_client.clone();
+
+        tokio::spawn(async move {
+            println!("Inside task");
+            let injured_starters = bad_starters_box.0;
+            println!("Injured starters len was 0! {}", injured_starters.len() == 0);
+
+            for injured_starter in &injured_starters {
+                println!("{:?}", injured_starter.name);
+                println!("{:?}", injured_starter.pos);
+                let content = messaging::create_injured_msg_string(
+                    None,
+                    username.clone(),
+                    injured_starter.name.as_ref(), 
+                    injured_starter.status.clone(),
+                    injured_starter.pos.clone());
+                
+                println!("Content is {content}");
+
+                match msg_client_copy.post_bot_message(content.as_ref()).await {
+                    Ok(_) => {
+                        println!("Posted message successfully!");
+                        ()
+                    },
+                    Err(e) => {
+                        println!("Fucksie wupsie!");
+                        eprintln!("{e}");
+                    }
+                }
             }
-        } else {
-            println!("->  No injured starters!");
-        }
-
-        if value.1.1.len() > 0 {
-            println!("->  Empty starters...");
-            for empty in &value.1.1 {
-                println!("    ->  {} is empty", empty);
-            }
-        } else {
-            println!("->  No empty starters!");
-        }
-
-        if value.1.2.len() > 0 {
-            println!("->  Starters on bye...");
-            for bye in &value.1.2 {
-                println!("    ->  {} - {} is on bye", bye.pos, bye.name);
-            }
-        } else {
-            println!("->  No starters on bye!");
-        }
+        })
     });
+
+
+    for task in tasks {
+        println!("Running task...");
+        task.await.expect("Task failed!");
+    }
+
+    println!("Hey, we're at the end");
 }
+
+// async fn message_invalid_starters(values: Vec<(&sleeper::data::SleeperUser, BadStarters)>, msg_client: &GroupmeClient) {
+//     values.iter().for_each(|value| {
+//         println!("OwnerID: {}", value.0.user_id);
+
+//         if value.1.0.len() > 0 {
+//             println!("->  Injured starters...");
+//             for inj in &value.1.0 {
+//                 tokio::spawn(async move {
+//                     let owner_name: String = value.0.username.unwrap_or(String::new());
+//                     let team_name: String = value.0.metadata.get("team_name").unwrap().unwrap();
+//                     let content: String = messaging::create_injured_msg_string(team_name.as_ref(), owner_name.as_ref(), inj.name.as_ref(), inj.status, inj.pos);
+//                     msg_client.post_bot_message(content.as_ref()).await.unwrap_or_else(|opt| {});
+//                     println!("    ->  {:?} - {:?} is {:?}", inj.pos, inj.name, inj.status);
+//                 });
+//             }
+//         } else {
+//             println!("->  No injured starters!");
+//         }
+
+//         if value.1.1.len() > 0 {
+//             println!("->  Empty starters...");
+//             for empty in &value.1.1 {
+//                 println!("    ->  {:?} is empty", empty);
+//             }
+//         } else {
+//             println!("->  No empty starters!");
+//         }
+
+//         if value.1.2.len() > 0 {
+//             println!("->  Starters on bye...");
+//             for bye in &value.1.2 {
+//                 println!("    ->  {:?} - {:?} is on bye", bye.pos, bye.name);
+//             }
+//         } else {
+//             println!("->  No starters on bye!");
+//         }
+//     });
+
+//     ()
+// }
 
--- src/messaging.rs
+++ src/messaging.rs
@@ -1,25 +1,56 @@
 use groupme::{ client::*, OutgoingBotMessage };
-use sleeper::data::{ InjuryStatus, RosterPosition };
+use sleeper::data::{ InjuryStatus, RosterPosition, OwnerId };
+
+use crate::brains::BadStarters;
 
 /// Creates the formatted message String in cases where the owner is starting an injured or
 /// otherwise inactive player at the given position
-pub fn create_injured_msg_string(team_name: &str,
-                                 owner_username: &str,
+pub fn create_injured_msg_string(team_name: Option<String>,
+                                 owner_username: String,
                                  player_name: &str,
                                  status: InjuryStatus,
                                  position: RosterPosition) -> String {
-    format!("⛔ Team {team_name} ({owner_username}) is starting {player_name} ({status}) at {position}! ⛔")
+    let team_name = match team_name {
+        Some(team_name) => team_name,
+        None => "".to_string()
+    };
+
+    let owner_username_string = match team_name.as_ref() {
+        "" => owner_username,  // No team name, so just say username
+        _  => format!("({owner_username})") // Team name present, owner in parentheses
+    };
+
+    format!("⛔ {team_name} {owner_username_string} is starting {player_name} ({status}) at {position}! ⛔")
 }
 
 /// Creates the formatted message String in cases where the owner is not starting a player at the
 /// given position
-pub fn create_empty_msg_string(team_name: &str, owner_username: &str, position: RosterPosition) -> String {
-    format!("🕳️ Team {team_name} ({owner_username}) is not starting a player at {position}! 🕳️")
+pub fn create_empty_msg_string(team_name: Option<String>, owner_username: String, position: RosterPosition) -> String {
+    let team_name = match team_name {
+        Some(team_name) => team_name,
+        None => "".to_string()
+    };
+
+    let owner_username_string = match team_name.as_ref() {
+        "" => owner_username,  // No team name, so just say username
+        _  => format!("({owner_username})") // Team name present, owner in parentheses
+    };
+
+    format!("🕳️ {team_name} {owner_username_string} is not starting a player at {position}! 🕳️")
 }
 
 /// Creates the formatted message String in cases where the owner is starting a player on bye at
 /// the given position
-pub fn create_bye_msg_string(team_name: &str, owner_username: &str, player_name: &str, position: RosterPosition) -> String {
-    format!("💤 Team {team_name} ({owner_username}) is starting {player_name} (on bye) at {position}! 💤")
+pub fn create_bye_msg_string(team_name: Option<String>, owner_username: String, player_name: &str, position: RosterPosition) -> String {
+    let team_name = match team_name {
+        Some(team_name) => team_name,
+        None => "".to_string()
+    };
+
+    let owner_username_string = match team_name.as_ref() {
+        "" => owner_username,  // No team name, so just say username
+        _  => format!("({owner_username})") // Team name present, owner in parentheses
+    };
+    format!("💤 {team_name} {owner_username_string} is starting {player_name} (on bye) at {position}! 💤")
 }
 
